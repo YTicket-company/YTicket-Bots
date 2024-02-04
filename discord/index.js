48efe39c2e60ecf3fe67d960a5120124dd739bb1
MazBaz <mrlog42@gmail.com>
2024-02-04 14:10:07 +0100

    added bot

--- discord/index.js
+++ discord/index.js
@@ -1,11 +1,13 @@
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Events} = require("discord.js");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+const Api = require('./api');
 const moment = require("moment");
+
 moment.locale("fr");
 
-const token =
-  "MTE5ODkyMTAzODk4NjIxNTU1NQ.GiqUZj.t4KTctKxreZL_wll8ACezoYYqfb4iqdCZ9RNbg";
+
+const token = "MTE5ODkyMTAzODk4NjIxNTU1NQ.GiqUZj.t4KTctKxreZL_wll8ACezoYYqfb4iqdCZ9RNbg";
 const clientId = "1198921038986215555";
 const guildId = "1198927880076410921";
 
@@ -25,105 +27,38 @@ const client = new Client({
   ],
 });
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
 
-  const prefix = "!";
-
-  if (message.guild) {
-    if (message.channel.name.startsWith("ticket-")) {
-      if (!message.content.startsWith(prefix)) {
-        console.log(`[${message.author.tag}]: ${message.content}`);
-      }
-    } else if (message.content.toLowerCase() === `${prefix}createticket`) {
-      try {
-        const ticketChannel = await message.guild.channels.create(
-          `ticket-${message.author.id}`,
-          {
-            type: "text",
-            permissionOverwrites: [
-              {
-                id: message.author.id,
-                allow: [
-                  Permissions.FLAGS.VIEW_CHANNEL,
-                  Permissions.FLAGS.SEND_MESSAGES,
-                ],
-              },
-              {
-                id: message.guild.roles.everyone,
-                deny: [Permissions.FLAGS.VIEW_CHANNEL],
-              },
-            ],
-          }
-        );
-
-        console.log(
-          `Canal créé : ${ticketChannel.name} ID : ${ticketChannel.id}`
-        );
-
-        const fetchedChannel = message.guild.channels.cache.get(
-          ticketChannel.id
-        );
-
-        fetchedChannel.on("messageCreate", (msg) => {
-          console.log(`[${msg.author.tag}]: ${msg.content}`);
-        });
-
-        console.log(
-          `Canal créé : ${ticketChannel.name} ID : ${ticketChannel.id}`
-        );
-
-        await ticketChannel.send(
-          `Bienvenue dans votre ticket, ${message.author}!`
-        );
-      } catch (error) {
-        console.error(`Erreur lors de la création du canal : ${error.message}`);
-        console.error(error.stack);
-      }
-    }
-  } else {
-    console.log(`[${message.author.tag}]: ${message.content}`);
-
-    if (message.content.toLowerCase() === `${prefix}createticket`) {
-      try {
-        const ticketChannel = await message.author.createDM();
-
-        console.log(
-          `Canal créé : ${ticketChannel.name} ID : ${ticketChannel.id}`
-        );
-
-        await ticketChannel.send(
-          `Bienvenue dans votre ticket, ${message.author}! `
-        );
-      } catch (error) {
-        console.error(`Erreur lors de la création du canal : ${error.message}`);
-        console.error(error.stack);
-      }
-    }
-  }
+client.once("ready", () => {
+    console.log("Bot on !")
 });
-
-client.once("ready", () => {});
 
 client.login(token);
 
-const rest = new REST({ version: "9" }).setToken(token);
+const api = new Api();
 
-const commands = [
-  {
-    name: "ping",
-    description: "Pong!",
-  },
-];
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot || message.guild) return;
 
-const registerCommands = async () => {
-  try {
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commands,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+    try {
+        const channel = await message.author.createDM();
 
-registerCommands();
+        console.log(`[${message.author.tag}] [${channel.id}]: ${message.content}`);
+
+        const client = await api.createClient(message.author.id, message.author.username);
+
+        if (client === null) {
+            console.error(`Failed to create client for ${message.author.username}.`);
+            return await channel.send("Oups :/ we can't create your ticket ! 😅");
+        }
+
+        let ticket = await api.getOpenedTicket(message.author.id);
+
+        if (ticket === null) {
+            ticket = await api.createTicket(client.name, message.author.id, message.channelId);
+            await channel.send(`${ticket.id} | Ticket created ! A staff will be back soon 😎`);
+        }
+    } catch (error) {
+        console.error(`Erreur lors de la création du canal : ${error.message}`);
+        console.error(error.stack);
+    }
+});
