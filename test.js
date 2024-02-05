e123577017c877753d12bd7ddcc69942903508e7
MazBaz <mrlog42@gmail.com>
2024-02-05 16:32:18 +0100

    aled

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log("adl");
 const WebSocket = require('ws');
 
-const socket = new WebSocket(`ws://yticket-wss.mazbaz.fr`);
+const socket = new WebSocket(`ws://yticket-wss.mazbaz.fr:8002`);
 
 socket.on('open', () => {
     console.log('[Discord] Connecté au serveur WebSocket');
